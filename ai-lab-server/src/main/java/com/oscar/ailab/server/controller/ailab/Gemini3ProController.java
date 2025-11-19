096ac479027562a9af165a8d170f6823ddd6a2d0
guoqiang yang <yangguoqiang@mafengwo.com>
2025-11-19 19:18:07 +0800

    fix: GetMapping

--- ai-lab-server/src/main/java/com/oscar/ailab/server/controller/ailab/Gemini3ProController.java
+++ ai-lab-server/src/main/java/com/oscar/ailab/server/controller/ailab/Gemini3ProController.java
@@ -3,6 +3,7 @@ package com.oscar.ailab.server.controller.ailab;
 import com.oscar.ailab.client.dto.ailab.ConfigResponseDTO;
 import com.oscar.ailab.server.domain.ResultWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/gemini3/pro")
 public class Gemini3ProController {
 
+    @GetMapping("/config")
     public ResultWrapper<ConfigResponseDTO> getConfig() {
         return ResultWrapper.success(ConfigResponseDTO.builder()
                 .llmName("Gemini3.5 Pro")
